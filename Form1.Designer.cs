ee36d3365899b44bb00e943f0685b61db2b22145
Viktor Blazhkevych <blazhkevych.va@gmail.com>
2020-12-12 00:32:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace _10122020dzAnketa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.dateTimePickerBirthday = new System.Windows.Forms.DateTimePicker();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.textBoxCountry = new System.Windows.Forms.TextBox();
+            this.labelCountry = new System.Windows.Forms.Label();
+            this.textBoxPatronymic = new System.Windows.Forms.TextBox();
+            this.labelPatronymic = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.buttonSeeResults = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Controls.Add(this.labelGender);
+            this.groupBox1.Controls.Add(this.dateTimePickerBirthday);
+            this.groupBox1.Controls.Add(this.labelBirthday);
+            this.groupBox1.Controls.Add(this.textBoxPhone);
+            this.groupBox1.Controls.Add(this.labelPhone);
+            this.groupBox1.Controls.Add(this.textBoxCity);
+            this.groupBox1.Controls.Add(this.labelCity);
+            this.groupBox1.Controls.Add(this.textBoxCountry);
+            this.groupBox1.Controls.Add(this.labelCountry);
+            this.groupBox1.Controls.Add(this.textBoxPatronymic);
+            this.groupBox1.Controls.Add(this.labelPatronymic);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Controls.Add(this.textBoxSurname);
+            this.groupBox1.Controls.Add(this.labelSurname);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 435);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Анкета";
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(297, 390);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(116, 29);
+            this.radioButtonFemale.TabIndex = 16;
+            this.radioButtonFemale.Text = "Женский";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Checked = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(174, 390);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(117, 29);
+            this.radioButtonMale.TabIndex = 15;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Мужской";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGender.Location = new System.Drawing.Point(6, 387);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(60, 25);
+            this.labelGender.TabIndex = 14;
+            this.labelGender.Text = "Пол :";
+            // 
+            // dateTimePickerBirthday
+            // 
+            this.dateTimePickerBirthday.Location = new System.Drawing.Point(174, 332);
+            this.dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            this.dateTimePickerBirthday.Size = new System.Drawing.Size(278, 30);
+            this.dateTimePickerBirthday.TabIndex = 13;
+            this.dateTimePickerBirthday.Value = new System.DateTime(2020, 12, 11, 0, 0, 0, 0);
+            // 
+            // labelBirthday
+            // 
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBirthday.Location = new System.Drawing.Point(6, 337);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(161, 25);
+            this.labelBirthday.TabIndex = 12;
+            this.labelBirthday.Text = "Дата рождения";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(115, 260);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(337, 30);
+            this.textBoxPhone.TabIndex = 11;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPhone.Location = new System.Drawing.Point(6, 263);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(99, 25);
+            this.labelPhone.TabIndex = 10;
+            this.labelPhone.Text = "Телефон";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(115, 222);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(337, 30);
+            this.textBoxCity.TabIndex = 9;
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCity.Location = new System.Drawing.Point(6, 225);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(69, 25);
+            this.labelCity.TabIndex = 8;
+            this.labelCity.Text = "Город";
+            // 
+            // textBoxCountry
+            // 
+            this.textBoxCountry.Location = new System.Drawing.Point(115, 181);
+            this.textBoxCountry.Name = "textBoxCountry";
+            this.textBoxCountry.Size = new System.Drawing.Size(337, 30);
+            this.textBoxCountry.TabIndex = 7;
+            // 
+            // labelCountry
+            // 
+            this.labelCountry.AutoSize = true;
+            this.labelCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCountry.Location = new System.Drawing.Point(6, 184);
+            this.labelCountry.Name = "labelCountry";
+            this.labelCountry.Size = new System.Drawing.Size(79, 25);
+            this.labelCountry.TabIndex = 6;
+            this.labelCountry.Text = "Страна";
+            // 
+            // textBoxPatronymic
+            // 
+            this.textBoxPatronymic.Location = new System.Drawing.Point(115, 110);
+            this.textBoxPatronymic.Name = "textBoxPatronymic";
+            this.textBoxPatronymic.Size = new System.Drawing.Size(337, 30);
+            this.textBoxPatronymic.TabIndex = 5;
+            // 
+            // labelPatronymic
+            // 
+            this.labelPatronymic.AutoSize = true;
+            this.labelPatronymic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPatronymic.Location = new System.Drawing.Point(6, 113);
+            this.labelPatronymic.Name = "labelPatronymic";
+            this.labelPatronymic.Size = new System.Drawing.Size(104, 25);
+            this.labelPatronymic.TabIndex = 4;
+            this.labelPatronymic.Text = "Отчество";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(115, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(337, 30);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelName.Location = new System.Drawing.Point(6, 74);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(54, 25);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Имя";
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(115, 31);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(337, 30);
+            this.textBoxSurname.TabIndex = 1;
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSurname.Location = new System.Drawing.Point(6, 34);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(103, 25);
+            this.labelSurname.TabIndex = 0;
+            this.labelSurname.Text = "Фамилия";
+            // 
+            // buttonSeeResults
+            // 
+            this.buttonSeeResults.Location = new System.Drawing.Point(12, 476);
+            this.buttonSeeResults.Name = "buttonSeeResults";
+            this.buttonSeeResults.Size = new System.Drawing.Size(458, 49);
+            this.buttonSeeResults.TabIndex = 1;
+            this.buttonSeeResults.Text = "ПРОСМОТРЕТЬ РЕЗУЛЬТАТЫ";
+            this.buttonSeeResults.UseVisualStyleBackColor = true;
+            this.buttonSeeResults.Click += new System.EventHandler(this.buttonSeeResults_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 553);
+            this.Controls.Add(this.buttonSeeResults);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Анкета";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.TextBox textBoxCountry;
+        private System.Windows.Forms.Label labelCountry;
+        private System.Windows.Forms.TextBox textBoxPatronymic;
+        private System.Windows.Forms.Label labelPatronymic;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthday;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Button buttonSeeResults;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _10122020dzAnketa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //buttonSeeResults.Click += ButtonSeeResults_Click;
+            //saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        //private void ButtonSeeResults_Click(object sender, EventArgs e)
+        //{
+
+        //    //if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+        //    //    return;
+        //    //// получаем выбранный файл
+        //    //string filename = saveFileDialog1.FileName;
+        //    //// сохраняем текст в файл
+        //    //System.IO.File.WriteAllText(filename, textBoxName.Text);
+        //    //MessageBox.Show("Файл сохранен");
+        //}
+
+        private void buttonSeeResults_Click_1(object sender, EventArgs e)
+        {
+            if (!File.Exists("questionnaires.txt"))
+                using (var sw = File.CreateText(Application.StartupPath + @"\" + "questionnaires.txt"))
+                {
+                    sw.WriteLine($"\t***Анкета создана {DateTime.Now}***");
+                    sw.WriteLine("Фамилия : " + textBoxSurname.Text);
+                    sw.WriteLine("Имя : " + textBoxName.Text);
+                    sw.WriteLine("Отчество : " + textBoxPatronymic.Text);
+                    sw.WriteLine("Страна : " + textBoxCountry.Text);
+                    sw.WriteLine("Город : " + textBoxCity.Text);
+                    sw.WriteLine("Телефон : " + textBoxPhone.Text);
+                    sw.WriteLine("Дата рождения : " + dateTimePickerBirthday.Text);
+
+                    var value = "";
+                    var isChecked = radioButtonFemale.Checked;
+                    if (isChecked)
+                        value = radioButtonFemale.Text;
+                    else
+                        value = radioButtonMale.Text;
+
+                    sw.WriteLine($"Пол : {value}");
+                    sw.WriteLine('\n');
+                }
+            else
+                using (var sw = File.AppendText(Application.StartupPath + @"\" + "questionnaires.txt"))
+                {
+                    sw.WriteLine($"\t***Анкета создана {DateTime.Now}***");
+                    sw.WriteLine("Фамилия : " + textBoxSurname.Text);
+                    sw.WriteLine("Имя : " + textBoxName.Text);
+                    sw.WriteLine("Отчество : " + textBoxPatronymic.Text);
+                    sw.WriteLine("Страна : " + textBoxCountry.Text);
+                    sw.WriteLine("Город : " + textBoxCity.Text);
+                    sw.WriteLine("Телефон : " + textBoxPhone.Text);
+                    sw.WriteLine("Дата рождения : " + dateTimePickerBirthday.Text);
+
+                    var value = "";
+                    var isChecked = radioButtonFemale.Checked;
+                    if (isChecked)
+                        value = radioButtonFemale.Text;
+                    else
+                        value = radioButtonMale.Text;
+
+                    sw.WriteLine($"Пол : {value}");
+                    sw.WriteLine('\n');
+                }
+            //обнуляем поля
+            textBoxSurname.Clear();
+            textBoxName.Clear();
+            textBoxPatronymic.Clear();
+            textBoxCountry.Clear();
+            textBoxCity.Clear();
+            textBoxPhone.Clear();
+            dateTimePickerBirthday.Value = DateTime.Now;
+        }
+    }
+}
